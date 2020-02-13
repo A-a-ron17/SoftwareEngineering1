a93ad5df15ac335089fe8ee37006e817bcd7f0cf
A-a-ron17 <60945297+A-a-ron17@users.noreply.github.com>
2020-02-13 15:44:56 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -17,7 +17,23 @@ int x_val = 10; //Obj.nextInt();
 int value = 0;
 int count = 1;
 
+while (x_val >= 1)
+{ 
 
+ val = (double)count/x;
+System.out.println(val);
+
+ value = value + x_val; 
+ count ++ ;
+
+ if (count == 5)
+ { 
+  x_val = 4;
+ }
+ else 
+{ 
+  x_val -= 2;
+}
 
 System.out.println("value"+value);  
 System.out.println("Count"+count);
